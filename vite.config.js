438a6bc7f54cbd1f1fec73f12c77064be082ad68
Sachinkumarsaho0 <124592113+Sachinkumarsaho0@users.noreply.github.com>
2024-02-29 18:31:47 +0530

    changes vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
-
 // export default defineConfig({
 //   plugins: [react()],
-//   server: {
-//     host: true,
-//     strictPort: true,
-//     port: 8000,
-//   },
 // });
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
+});
